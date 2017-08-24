8b98d7bab014251f23453caa5b0334ccc9619248
Stanley Elliott <s3659302@student.rmit.edu.au>
2017-08-24 12:07:04 +1000

    Refactor testing code into smaller methods

--- src/test/UserAccountSystemTest.java
+++ src/test/UserAccountSystemTest.java
@@ -2,30 +2,40 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import main.UserAccountInterface;
 import main.UserAccountSystem;
 
 public class UserAccountSystemTest {
-
-	@Test
-	public void test() {
-		int[] ids = new int[10];
-		UserAccountSystem accountSystem = new UserAccountSystem();
-		UserAccountInterface[] accounts = new UserAccountInterface[10];
-		int[] orderToCheck = {0,2,5,3,7,9,8,1,4,6,0,4,3,5,6};
-		for(int i = 0; i < 10; i++) {
-			UserAccountInterface eachAccount = new DummyAccount();
-			accounts[i] = eachAccount;
-			int eachID = accountSystem.add(eachAccount);
-			ids[i] = eachID;
-		}
-		for(int i = 0; i<orderToCheck.length; i++) {
-			int eachID = ids[i];
-			UserAccountInterface eachAccount = accounts[i];
-			assertEquals(accountSystem.getByID(eachID),eachAccount);
-		}
+	
+	private UserAccountSystem accountSystem;
+	@Before
+	public void setUp() throws Exception {
+		accountSystem = new UserAccountSystem();
 	}
-
+	
+	@Test
+	public void testAccountsAreAccessiblyById() {
+		UserAccountInterface newAccount = new DummyAccount();
+		int id = accountSystem.add(newAccount);
+		assertSame(accountSystem.getByID(id),newAccount);
+	}
+	@Test
+	public void testAccountsAreAccessiblyInDifferentOrders() {
+		UserAccountInterface[] accounts = new UserAccountInterface[5];
+		int[] ids = new int[5];
+		int[] orderToCheck = {0,2,1,3,2,4,4,3};
+		for(int i = 0; i<10; i++) {
+			accounts[i] = new DummyAccount();
+			ids[i] = accountSystem.add(accounts[i]);
+		}
+		for (int i = 0; i<orderToCheck.length; i++) {
+			int j = orderToCheck[i];
+			assertSame(accounts[j],accountSystem.getByID(ids[j]));
+		}
+		
+	}
 }
+
