1a853b0cc3e88010fc2f2d4eea7bb56b5d0bd918
alex_j <alex.jon.jarvis@gmail.com>
2017-08-24 12:14:09 +1000

    Add test: checks StaffAccount permissions handled correctly

---
+++ src/test/StaffAccountTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import main.StaffAccountSystem;
+
+public class StaffAccountSystemTest
+{
+    private StaffAccount acccount;
+
+    @Before
+    public void setup()
+    {
+        account = new StaffAccount("Firstname LastName");
+    }
+
+
+    @Test
+    public void testSettingPermissions()
+    {
+        Permission accountPermission = Permission.STAFF;
+
+        assertEquals(account.getPermission(), accountPermission);
+        account.setPermission(Permission.STAFF);
+        assertEquals(account.getPermission(), accountPermission);
+    }
+
+    @Test
+    public void testSettingInvalidPermissions()
+    {
+
+    }
+}
