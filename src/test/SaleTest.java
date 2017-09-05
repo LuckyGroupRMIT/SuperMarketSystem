07038a92089f1afe53d20ffe0835a6378583385e
rmit-s3607170-Alex-Jarvis <s3607170@student.rmit.edu.au>
2017-09-06 08:00:22 +1000

    modified SaleTest class

--- src/test/SaleTest.java
+++ src/test/SaleTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import main.Product;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,14 +11,23 @@ import main.Sale;
 public class SaleTest {
 
     private Sale sale;
+    private Product product;
 
     @Before
     public void setUp() {
         sale = new Sale();
+        product = new Product();
+    }
+
+    @Test
+    public void testAddPurchase() {
+        sale.addPurchase(product, 5);
+        assertEquals(5, sale.getSaleQuantity());
     }
 
     @Test
     public void testCancelPurchase() {
+        sale.addPurchase(product, 5);
         sale.cancelOrder();
         assertEquals(0, sale.getSaleQuantity());
     }
