80b64ec33e6201b745088cf8c7087a277f7a0250
rmit-s3656044-sean-chee <30304798+rmit-s3656044-sean-chee@users.noreply.github.com>
2017-09-07 11:22:32 +1000

    new classes

---
+++ Accounts.java
@@ -0,0 +1,12 @@
+
+public class Accounts {
+	private int username;
+	private String password;
+	private int permission;
+	
+	public Accounts(int username, String password, int permission){
+		this.username = username;
+		this.password = password;
+		this.permission = permission;
+	}
+}
