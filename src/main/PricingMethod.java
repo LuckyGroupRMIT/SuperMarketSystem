395c1c54385a1e6a97dd6c873f597fa8579dcb84
rmit-s3659302-stanley-elliott <s3659302@student.rmit.edu.au>
2017-09-25 12:05:03 +1000

    Finished Purchase Class

--- src/main/PricingMethod.java
+++ src/main/PricingMethod.java
@@ -1,5 +1,7 @@
 package main;
-
+// Quantity refers to the number of items purchased
+// Weight refers to the weight in grams
+// Both are represented using Ints.
 public enum PricingMethod {
 	WEIGHT, QUANTITY
 }
